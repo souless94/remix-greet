e9b60a106646bf1ead2c66e3e55a274d9b1ccfb9
souless94 <clwk94@gmail.com>
2025-01-27 22:32:57 +0800

    update app
    Signed-off-by: souless94 <clwk94@gmail.com>

--- app/root.tsx
+++ app/root.tsx
@@ -1,12 +1,14 @@
 import {
+  Link,
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useRouteError,
+  isRouteErrorResponse
 } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
-
 import "./tailwind.css";
 
 export const links: LinksFunction = () => [
@@ -18,8 +20,13 @@ export const links: LinksFunction = () => [
   },
   {
     rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    href: "https://fonts.googleapis.com/css2?family=Merriweather:wght@300&display=swap",
   },
+  {
+    rel: "stylesheet",
+    href: "https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap",
+  },
+  { rel: "icon", href: "/favicon.ico", type: "image/x-icon" }
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
@@ -42,4 +49,47 @@ export function Layout({ children }: { children: React.ReactNode }) {
 
 export default function App() {
   return <Outlet />;
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error)) {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-gray-100 text-center">
+        <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full">
+          {error.status === 404 ? (
+            <>
+              <h2 className="text-3xl font-bold text-red-500">404 - Page Not Found</h2>
+              <p className="mt-4 text-lg text-gray-700 mb-5">Sorry, we couldn't find the page you were looking for.</p>
+              <Link to="/" className="px-4 py-2 bg-transparent text-green-500 border-2 border-green-500 rounded-lg hover:bg-green-500 hover:text-white focus:outline-none transition-all duration-300 ease-in-out">
+                Go Back to Home
+              </Link>
+            </>
+          ) : (
+            <>
+              <h2 className="text-3xl font-bold text-red-500">Oops! Something went wrong.</h2>
+              <p className="mt-4 text-lg text-gray-700 mb-5">An unexpected error occurred. Please try again later.</p>
+              <Link to="/" className="px-4 py-2 bg-transparent text-green-500 border-2 border-green-500 rounded-lg hover:bg-green-500 hover:text-white focus:outline-none transition-all duration-300 ease-in-out">
+                Go Back to Home
+              </Link>
+            </>
+          )}
+        </div>
+      </div>
+    );
+  }
+
+  // You can optionally handle non-Route errors here as a fallback, if needed
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 text-center">
+      <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full">
+        <h2 className="text-3xl font-bold text-red-500">Something went wrong!</h2>
+        <p className="mt-4 text-lg text-gray-700 mb-5">We encountered an issue. Please try again later.</p>
+        <Link to="/" className="px-4 py-2 bg-transparent text-green-500 border-2 border-green-500 rounded-lg hover:bg-green-500 hover:text-white focus:outline-none transition-all duration-300 ease-in-out">
+          Go Back to Home
+        </Link>
+      </div>
+    </div>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,7 @@ export default {
           "Segoe UI Symbol",
           "Noto Color Emoji",
         ],
+        serif: ["Merriweather", "serif"],
       },
     },
   },
